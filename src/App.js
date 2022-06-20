bf393628dc5bb705ed4abfccec904a0fff98513f
Pappu Kumar <digitalpappu101@gmail.com>
2022-06-20 21:57:24 +0530

    sprint3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navbar from './components/Navbar'
 
 function App() {
-  return <div className="App">{/* CODE HERE */}</div>;
+  return <div className="App">{/* CODE HERE */
+  
+          <Navbar/>  
+  
+  
+  
+  }</div>;
 }
 
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Products = () => {
-  return <div>{/* CODE HERE */}</div>;
+    
+  const [product,setProduct]=useState([]);
+
+  React.useState(() => {
+      fetch(` http://localhost:8080/cartItems`)
+      .then((res) =>res.json())
+      .then((res)=>setProduct(res))
+      .catch((err) =>console.log(err))
+
+  })
+
+
+
+
+  return <div>
+     {product.map((prod)=>{
+      return(
+        <>
+        <h1>{prod.name}</h1>
+        <p>{prod.description}</p>
+        <button onClick={()=>{}}>ADD</button>
+        <button onClick={()=>{}}>DECREASE</button>
+        <button onClick={()=>{}}>Remove</button>
+        </>
+      )
+     })}
+  </div>;
 };
 
 export default Products;
