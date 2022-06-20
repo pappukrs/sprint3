bf27a5d276b62d848ec4b0652c01510afeee53a5
Pappu Kumar <digitalpappu101@gmail.com>
2022-06-20 21:56:59 +0530

    sprint3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from 'styled-components'
+import {Link} from  'react-router-dom'
 
+
+
+const NavCss=styled.div`
+   
+  display: flex;
+  justify-content: space-between;
+  background: teal;
+  gap: 18px;
+  color: white;
+  
+`
 const Navbar = () => {
-  return <div>{/* CODE HERE */}</div>;
+  return <NavCss>
+    <Link>HOME</Link>
+    <Link to='/login'>LOGIN</Link>
+
+
+  </NavCss>;
 };
 
 export default Navbar;
