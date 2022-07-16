4160b5ab1dc746a8cbfaf343691718a7e099ed94
Pappu Kumar <90520097+pappukrs@users.noreply.github.com>
2022-07-17 01:26:14 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,34 +1,5 @@
 import React from "react";
 
-const Products = () => {
-    
-  const [product,setProduct]=useState([]);
-
-  React.useState(() => {
-      fetch(` http://localhost:8080/cartItems`)
-      .then((res) =>res.json())
-      .then((res)=>setProduct(res))
-      .catch((err) =>console.log(err))
-
-  })
 
 
-
-
-  return <div>
-     {product.map((prod)=>{
-      return(
-        <>
-        <h1>{prod.name}</h1>
-        <p>{prod.description}</p>
-        <button onClick={()=>{}}>ADD</button>
-        <button onClick={()=>{}}>DECREASE</button>
-        <button onClick={()=>{}}>Remove</button>
-        <button onClick={()=>{}}>Remove</button>
-        </>
-      )
-     })}
-  </div>;
-};
-
-export default Products;
+ 
