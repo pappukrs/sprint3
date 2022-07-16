d09875e8b31e1ff93ee2daf3a4992ce82ae0ed63
Pappu Kumar <90520097+pappukrs@users.noreply.github.com>
2022-07-17 01:25:34 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,8 @@
 import React from "react";
-import styled from 'styled-components'
-import {Link} from  'react-router-dom'
 
 
 
-const NavCss=styled.div`
-   
-  display: flex;
-  justify-content: space-between;
-  background: teal;
-  gap: 18px;
-  color: white;
+
   
-`
-const Navbar = () => {
-  return <NavCss>
-    <Link>HOME</Link>
-    <Link to='/login'>LOGIN</Link>
 
 
-  </NavCss>;
-};
-
-export default Navbar;
