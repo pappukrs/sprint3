2565d45e632b6fa46252ec8ce0620f9f49f38eb2
Pappu Kumar <90520097+pappukrs@users.noreply.github.com>
2022-07-17 01:27:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
 import "./App.css";
 import Navbar from './components/Navbar'
 
-function App() {
-  return <div className="App">{/* CODE HERE */
-  
-          <Navbar/>  
-  
-  
-  
-  }</div>;
-}
-
-export default App;
